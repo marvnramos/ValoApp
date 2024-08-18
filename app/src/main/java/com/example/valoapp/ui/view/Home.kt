6462368977ef92fd896a5f30f93cd9c1ebd1f688
Marvin Ramos <mr487223@gmail.com>
2024-08-18 14:42:42 -0600

    feat(HomeView): add auto load and generate agents cards

--- app/src/main/java/com/example/valoapp/ui/view/Home.kt
+++ app/src/main/java/com/example/valoapp/ui/view/Home.kt
@@ -1,13 +1,77 @@
 package com.example.valoapp.ui.view
 
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.valoapp.data.models.CardData
 import com.example.valoapp.ui.view.components.CardComponent
+import com.example.valoapp.ui.viewmodel.AgentsViewModel
+
 
 @Preview
 @Composable
-fun HomeView() {
-    CardComponent()
+fun HomeView(viewModel: AgentsViewModel = viewModel()) {
+    val context = LocalContext.current
+
+    val agents by viewModel.agents.observeAsState()
+    val isLoading by viewModel.isLoading.observeAsState(true)
+    val errorMessage by viewModel.errorMessage.observeAsState()
+
+    if (errorMessage != null) {
+        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    val agentList = agents?.data ?: emptyList()
+
+//    Button(
+//        colors = CardDefaults.cardColors(containerColor = Color(0x3a7233ff)),
+//        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+//        shape = RoundedCornerShape(16.dp),
+//        modifier = Modifier
+//            .weight(1f)
+//            .padding(8.dp),
+//        onClick = { /*TODO*/ }) {
+//
+//    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(agentList.chunked(2)) { agentPair ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                agentPair.forEach { agent ->
+                    val dataCard = CardData(
+                        agent = agent,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(8.dp),
+                        onClick = {
+                            Toast.makeText(context, agent.uuid, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                    CardComponent(dataCard)
+                }
+            }
+        }
+    }
 }
