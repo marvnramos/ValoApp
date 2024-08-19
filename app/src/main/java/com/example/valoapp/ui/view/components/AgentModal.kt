2194cb9ce9f87cb48a1b8eb99ec218e86800dc82
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-19 10:02:07 -0600

    refactor(dialog): add agent modal logic

--- app/src/main/java/com/example/valoapp/ui/view/components/AgentModal.kt
+++ app/src/main/java/com/example/valoapp/ui/view/components/AgentModal.kt
@@ -1,10 +1,84 @@
 package com.example.valoapp.ui.view.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+import com.example.valoapp.data.models.ModalData
+import com.example.valoapp.ui.viewmodel.AgentViewModel
 
-@Preview
 @Composable
-fun AgentModal(){
+fun AgentModal(data: ModalData) {
+    val (agentUUID, onDismissRequest, imageDescription) = data
 
+    val viewModel: AgentViewModel = viewModel()
+    val agentResponse by viewModel.agentResponse.observeAsState()
+
+    LaunchedEffect(agentUUID) {
+        viewModel.fetchAgent(agentUUID)
+    }
+
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(375.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                agentResponse?.let { agent ->
+                    Image(
+                        painter = rememberAsyncImagePainter(agent.data.bustPortrait),
+                        contentDescription = imageDescription,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .height(160.dp)
+                    )
+                    Text(
+                        text = agent.data.displayName,
+                        modifier = Modifier.padding(16.dp),
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        TextButton(
+                            onClick = { onDismissRequest() },
+                            modifier = Modifier.padding(8.dp),
+                        ) {
+                            Text("Dismiss")
+                        }
+                    }
+                } ?: run {
+                    Text(text = "Loading...")
+                }
+            }
+        }
+    }
 }
