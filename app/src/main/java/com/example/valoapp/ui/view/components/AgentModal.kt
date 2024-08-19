4fb8b8fcd2f05c44eefc1faaebfddcf7de609993
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-19 15:02:06 -0600

    refact(modal): add agent image

--- app/src/main/java/com/example/valoapp/ui/view/components/AgentModal.kt
+++ app/src/main/java/com/example/valoapp/ui/view/components/AgentModal.kt
@@ -1,17 +1,22 @@
 package com.example.valoapp.ui.view.components
 
+import androidx.compose.ui.graphics.Color
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -20,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -28,6 +34,20 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.example.valoapp.data.models.ModalData
 import com.example.valoapp.ui.viewmodel.AgentViewModel
+import com.example.valoapp.utils.hexToColorInt
+
+//@Composable
+//@Preview
+//fun modalpreview(){
+//    var showDialog by remember { mutableStateOf(true) }
+//    if (showDialog) {
+//        AgentModal(data = ModalData(
+//            agentUUID = "dade69b4-4f5a-8528-247b-219e5a1facd6",
+//            onDismissRequest = { showDialog = false},
+//            imageDescription = "agent image"
+//        ))
+//    }
+//}
 
 @Composable
 fun AgentModal(data: ModalData) {
@@ -47,7 +67,6 @@ fun AgentModal(data: ModalData) {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(375.dp)
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
@@ -61,24 +80,83 @@ fun AgentModal(data: ModalData) {
                     isLoading -> {
                         Text(text = "Loading...")
                     }
+
                     errorMessage != null -> {
                         Log.d("AgentModal", "Error: $errorMessage")
                         Toast.makeText(context, "Something went wrong!", Toast.LENGTH_SHORT).show()
                         onDismissRequest()
                     }
+
                     agentResponse != null -> {
                         val agent = agentResponse!!.data
-                        Image(
-                            painter = rememberAsyncImagePainter(agent.bustPortrait),
-                            contentDescription = imageDescription,
-                            contentScale = ContentScale.Fit,
+                        val colors = agent?.backgroundGradientColors
+                        val colorList = mutableListOf<Color>()
+
+                        if (colors != null) {
+                            for (color in colors) {
+                                val parsedColor = hexToColorInt(color)
+                                colorList.add(Color(parsedColor))
+                            }
+                        }
+
+                        Row(
                             modifier = Modifier
-                                .height(160.dp)
-                        )
-                        Text(
-                            text = agent.displayName,
-                            modifier = Modifier.padding(16.dp),
-                        )
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                shape = RoundedCornerShape(16.dp),
+                                elevation = CardDefaults.cardElevation(10.dp)
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .background(
+                                            brush = Brush.verticalGradient(colors = colorList),
+                                            shape = RoundedCornerShape(16.dp)
+                                        )
+                                        .height(200.dp)
+                                ) {
+                                    Image(
+                                        painter = rememberAsyncImagePainter(agent.bustPortrait),
+                                        contentDescription = imageDescription,
+
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(200.dp),
+                                        contentScale = ContentScale.Crop
+                                    )
+                                }
+//                                Image(
+//                                    painter = rememberAsyncImagePainter(agent.bustPortrait),
+//                                    contentDescription = imageDescription,
+//                                    contentScale = ContentScale.Fit,
+//                                    modifier = Modifier
+//                                        .height(160.dp)
+//                                )
+                            }
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+//                                horizontalAlignment = Arrangement.
+                            ) {
+                                Text(
+                                    text = agent.displayName,
+                                    modifier = Modifier.padding(16.dp),
+                                )
+                                Text(
+                                    text = "Description:",
+                                    modifier = Modifier.padding(16.dp),
+                                )
+                                Text(
+                                    text = agent.description,
+                                    modifier = Modifier.padding(16.dp),
+                                )
+                            }
+                        }
+
+
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth(),
@@ -92,6 +170,7 @@ fun AgentModal(data: ModalData) {
                             }
                         }
                     }
+
                     else -> {
                         Text(text = "No data available.")
                     }
