c2638c974795d967bef72e686bc0e6f88b2c96dd
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-19 10:02:38 -0600

    refactor(viewmodel): add fetch data on agent view model

--- app/src/main/java/com/example/valoapp/ui/viewmodel/AgentsViewModel.kt
+++ app/src/main/java/com/example/valoapp/ui/viewmodel/AgentsViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.valoapp.data.RetrofitInstance
-import com.example.valoapp.data.models.Agents
+import com.example.valoapp.data.models.AgentsResponse
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class AgentsViewModel : ViewModel() {
-    private val _agents = MutableLiveData<Agents?>()
-    val agents: LiveData<Agents?> = _agents
+    private val _agentsResponse = MutableLiveData<AgentsResponse?>()
+    val agentsResponse: LiveData<AgentsResponse?> = _agentsResponse
 
     private val _isLoading = MutableLiveData(true)
     val isLoading: LiveData<Boolean> = _isLoading
@@ -29,17 +29,21 @@ class AgentsViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val data = RetrofitInstance.ApiClient.getAgents()
-                data.enqueue(object : Callback<Agents> {
-                    override fun onResponse(call: Call<Agents>, response: Response<Agents>) {
+                data.enqueue(object : Callback<AgentsResponse> {
+                    override fun onResponse(
+                        call: Call<AgentsResponse>,
+                        response: Response<AgentsResponse>
+                    ) {
                         _isLoading.value = false
                         if (response.isSuccessful) {
-                            _agents.value = response.body()
+                            _agentsResponse.value = response.body()
                         } else {
-                            _errorMessage.value = "Doesn't work: ${response.code()} ${response.message()}"
+                            _errorMessage.value =
+                                "Doesn't work: ${response.code()} ${response.message()}"
                         }
                     }
 
-                    override fun onFailure(call: Call<Agents>, t: Throwable) {
+                    override fun onFailure(call: Call<AgentsResponse>, t: Throwable) {
                         _isLoading.value = false
                         _errorMessage.value = "Failed to fetch data"
                     }
