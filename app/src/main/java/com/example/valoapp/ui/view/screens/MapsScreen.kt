18a447b2bdb572e5196fdd19ab1efc4b2bd6cd21
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-20 14:43:04 -0600

    feat(maps cards): add maps card view

--- app/src/main/java/com/example/valoapp/ui/view/screens/MapsScreen.kt
+++ app/src/main/java/com/example/valoapp/ui/view/screens/MapsScreen.kt
@@ -1,54 +1,92 @@
 package com.example.valoapp.ui.view.screens
 
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.valoapp.ui.viewmodel.MapsViewModel
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.valoapp.ui.view.components.MapCardComponent
+import com.example.valoapp.ui.viewmodel.MapsViewModel
+import com.example.valoapp.data.models.maps.Map
 
-@Preview
 @Composable
 fun MapsScreen(viewModel: MapsViewModel = viewModel()) {
     val maps by viewModel.mapsResponse.observeAsState()
     val isLoading by viewModel.isLoading.observeAsState(true)
     val errorMessage by viewModel.errorMessage.observeAsState()
 
+    val mapsList = maps?.data ?: emptyList()
     val context = LocalContext.current
 
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
+    Column(modifier = Modifier.fillMaxSize()) {
         when {
             isLoading -> {
-                Text(text = "Loading... ‍🌫️")
+                LoadingIndicator()
             }
 
-            !isLoading && errorMessage != null -> {
-                Log.d("Map cards", "Error: $errorMessage")
-                Toast.makeText(context, "Bad internet signal!", Toast.LENGTH_SHORT).show()
-
+            errorMessage != null -> {
+                ErrorMessage(context, errorMessage!!)
             }
 
             maps != null -> {
-                Text(text = "Maps view ✌️ ${maps!!.data.size}", color = Color.Black)
+                MapsList(mapsList)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoadingIndicator() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Loading...😶‍🌫️")
+    }
+}
+
+@Composable
+fun ErrorMessage(context: Context, errorMessage: String) {
+    Log.d("Map cards", "Error: $errorMessage")
+    Toast.makeText(context, "Bad internet signal!", Toast.LENGTH_SHORT).show()
+    Text(
+        text = "Error loading maps: $errorMessage",
+        modifier = Modifier.padding(16.dp),
+        color = androidx.compose.ui.graphics.Color.Red
+    )
+}
+
+@Composable
+fun MapsList(mapsList: List<Map>) {
+    LazyColumn(
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(mapsList.chunked(2)) { mapPair ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                mapPair.forEach { map ->
+                    MapCardComponent(map, modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp)
+                        .aspectRatio(1f)
+                    )
+                }
             }
         }
     }
