1dbc2030b3f20aca4ed72d97479a2aae730a201f
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-20 11:40:16 -0600

    feat(maps): add map endpoint

--- app/src/main/java/com/example/valoapp/data/repository/APIRepository.kt
+++ app/src/main/java/com/example/valoapp/data/repository/APIRepository.kt
@@ -2,6 +2,8 @@ package com.example.valoapp.data.repository
 
 import com.example.valoapp.data.models.agents.AgentResponse
 import com.example.valoapp.data.models.agents.AgentsResponse
+import com.example.valoapp.data.models.maps.MapResponse
+import com.example.valoapp.data.models.maps.MapsResponse
 import retrofit2.http.GET
 import retrofit2.Call
 import retrofit2.http.Path
@@ -11,4 +13,8 @@ interface APIRepository{
     fun getAgents(): Call<AgentsResponse>
     @GET("agents/{uuid}")
     fun getAgentById(@Path("uuid") uuid: String): Call<AgentResponse>
+    @GET("maps")
+    fun getMaps(): Call<MapsResponse>
+    @GET("agents/{uuid}")
+    fun getMapById(@Path("uuid") uuid: String): Call<MapResponse>
 }
