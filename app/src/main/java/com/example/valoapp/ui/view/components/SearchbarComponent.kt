19107b05c3143dc60930385ab689fac2d8579b9b
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-20 09:04:39 -0600

    refact(params): add mutablestate string param

--- app/src/main/java/com/example/valoapp/ui/view/components/SearchbarComponent.kt
+++ app/src/main/java/com/example/valoapp/ui/view/components/SearchbarComponent.kt
@@ -1,37 +1,27 @@
 package com.example.valoapp.ui.view.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun SearchBarSample() {
-    var text by rememberSaveable { mutableStateOf("") }
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
+fun SearchBarSample(text: MutableState<String>) {
     Column(
         Modifier
             .fillMaxWidth()
             .semantics { isTraversalGroup = true }) {
         Spacer(modifier = Modifier.height(8.dp))
         SearchBar(
-            query = text,
-            onQueryChange = { text = it },
+            query = text.value,
+            onQueryChange = { text.value = it },
             onSearch = {  },
             active = false,
             onActiveChange = { },
