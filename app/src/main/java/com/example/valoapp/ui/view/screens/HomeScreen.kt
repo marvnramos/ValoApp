d17b5706ebdef4bc0831354662922b6882c8be79
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-20 08:57:11 -0600

    feat(searchbar): add searchbar into HomeScreen

--- app/src/main/java/com/example/valoapp/ui/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/valoapp/ui/view/screens/HomeScreen.kt
@@ -1,20 +1,26 @@
 package com.example.valoapp.ui.view.screens
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,14 +31,13 @@ import com.example.valoapp.data.models.CardData
 import com.example.valoapp.data.models.ModalData
 import com.example.valoapp.ui.view.components.AgentModal
 import com.example.valoapp.ui.view.components.CardComponent
+import com.example.valoapp.ui.view.components.SearchBarSample
 import com.example.valoapp.ui.viewmodel.AgentsViewModel
 
 
 @Preview
 @Composable
 fun HomeScreen(viewModel: AgentsViewModel = viewModel()) {
-    val context = LocalContext.current
-
     val agents by viewModel.agentsResponse.observeAsState()
     val isLoading by viewModel.isLoading.observeAsState(true)
     val errorMessage by viewModel.errorMessage.observeAsState()
@@ -40,35 +45,57 @@ fun HomeScreen(viewModel: AgentsViewModel = viewModel()) {
     var showDialog by remember { mutableStateOf(false) }
     var selectedAgentUUID by remember { mutableStateOf("") }
 
-    if (!isLoading) {
-        errorMessage?.let {
-            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
-            return
-        }
-    }
+    val context = LocalContext.current
     val agentList = agents?.data ?: emptyList()
 
-    LazyColumn(
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(agentList.chunked(2)) { agentPair ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                agentPair.forEach { agent ->
-                    val dataCard = CardData(
-                        agent = agent,
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(8.dp)
-                            .height(250.dp),
-                        onClick = {
-                            selectedAgentUUID = agent.uuid
-                            showDialog = true
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        SearchBarSample()
+
+        when {
+            isLoading -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "Loading...")
+                }
+            }
+
+            errorMessage != null -> {
+                Log.d("Agent Cards", "Error: $errorMessage")
+                Toast.makeText(context, "Bad internet signal!", Toast.LENGTH_SHORT).show()
+            }
+
+            agents != null -> {
+                LazyColumn(
+                    contentPadding = PaddingValues(8.dp),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(agentList.chunked(2)) { agentPair ->
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            agentPair.forEach { agent ->
+                                val dataCard = CardData(
+                                    agent = agent,
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(8.dp)
+                                        .height(250.dp),
+                                    onClick = {
+                                        selectedAgentUUID = agent.uuid
+                                        showDialog = true
+                                    }
+                                )
+                                CardComponent(dataCard)
+                            }
                         }
-                    )
-                    CardComponent(dataCard)
+                    }
                 }
             }
         }
