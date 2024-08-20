466a31cc79de01f1bf8e4d89bdd1e269ca1ec5c7
Marvin Ramos <mframos@innovacion.gob.sv>
2024-08-20 09:05:46 -0600

    refact(params): add text param on SearchBar

--- app/src/main/java/com/example/valoapp/ui/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/valoapp/ui/view/screens/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.example.valoapp.ui.view.screens
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -19,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,7 +26,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberAsyncImagePainter
 import com.example.valoapp.data.models.CardData
 import com.example.valoapp.data.models.ModalData
 import com.example.valoapp.ui.view.components.AgentModal
@@ -44,13 +43,14 @@ fun HomeScreen(viewModel: AgentsViewModel = viewModel()) {
 
     var showDialog by remember { mutableStateOf(false) }
     var selectedAgentUUID by remember { mutableStateOf("") }
+    val text = rememberSaveable { mutableStateOf("") }
 
     val context = LocalContext.current
     val agentList = agents?.data ?: emptyList()
 
 
     Column(modifier = Modifier.fillMaxSize()) {
-        SearchBarSample()
+        SearchBarSample(text)
 
         when {
             isLoading -> {
